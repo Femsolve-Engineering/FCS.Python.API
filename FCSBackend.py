ae85ee6d0e9cca14a260e67921359ddbd5bbdf99
Tamas Balogh <tbalogh@pelogic.io>
2022-10-16 14:10:42 +0200

    22.10.15.1

--- FCSBackend.py
+++ FCSBackend.py
@@ -1,7 +1,9 @@
-
+"""
+22.10.15.1 - First API version checking. 
+"""
 # Versioning check
 from PyFCS import check_api_compatibility, get_backend_api_version
-FCS_PYTHON_API_VERSION = "22.10.15"
+FCS_PYTHON_API_VERSION = "22.10.15.1"
 if not check_api_compatibility(FCS_PYTHON_API_VERSION):
     raise Exception(f"Incompatible backend API!\n"
                    f"Please make sure that a major version of {get_backend_api_version()} is used.")
