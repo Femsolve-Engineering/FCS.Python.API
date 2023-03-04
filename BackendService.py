5a3c98f0b1b366013fae40f72b285400bff9b502
Tamas Balogh <tbalogh@pelogic.io>
2023-03-04 12:13:19 +0100

    Made FCSViewer instance user bound. If dev mode is on 'local' user is set.

--- BackendService.py
+++ BackendService.py
@@ -1,3 +1,8 @@
+
+from FCSViewer import FCSViewer
+from FCSViewer import GeometryBuilder, gb
+from FCSViewer import DocumentBuilder
+
 class BackendService(object):
     """
     Template class for hosting specific plugins.
@@ -8,26 +13,41 @@ class BackendService(object):
         Constructor.
         """
         self.app_guid = app_guid
+        self.fv: FCSViewer
+        self.db: DocumentBuilder
+        self.gb: GeometryBuilder
 
-    def set_existing_services(self, fcs_viewer, geometry_builder, document_builder) -> None: 
+    def set_existing_services(self, fcs_viewer: FCSViewer) -> None: 
         """
         To any backend service connect we pass on the instances of the main operators.
         """
 
         self.fv = fcs_viewer
-        self.gb = geometry_builder
-        self.db = document_builder
+        self.gb = gb
+        self.db = self.fv.db
+
+    def run_command(self, command_name: str, command_args: dict={}) -> bool:
+        """
+        Returns true, if the command was found and run (even if it failed).
+        Return false otherwise.
+        """
+        try:
+            command_ptr = getattr(self, command_name)
+            command_ptr(command_args)
+        except AttributeError:
+            print(f'Could not find {command_name}!')
+            return False
+        except Exception as ex:
+            print(f'Something failed: {ex.args}!')
+        finally:
+            return True
+
+#--------------------------------------------------------------------------------------------------
+# Pure virtual methods that require implementation
+#--------------------------------------------------------------------------------------------------
 
     def get_available_callbacks(self) -> list:
         """
         List of available callbacks to be forwarded to the listeners of the cloud application.
         """
         raise NotImplementedError("`get_available_callbacks` needs to be implemented in the base class!")
-
-
-    def run_command(self, command_name: str) -> bool:
-        """
-        Returns true, if the command was found and run (even if it failed).
-        Return false otherwise.
-        """
-        raise NotImplementedError("`run_command` needs to be implemented in the base class!")
