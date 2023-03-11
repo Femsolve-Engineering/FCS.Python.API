7ca3118e76c409eab26fbe1222dd6f699650f64e
TamasBalogh <tamasbalogh95@gmail.com>
2023-03-11 10:02:36 +0100

    Save log file path in FCSLogger.

--- FCSLogger.py
+++ FCSLogger.py
@@ -9,6 +9,7 @@ class FCSLogger:
     def __init__(self, user_id: str, path_to_log_file: str):
         self.logger = logging.getLogger(f'fcs_{user_id}')
         self.logger.setLevel(logging.INFO)
+        self.path_to_log_file = path_to_log_file
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
         self.stream_handler = logging.StreamHandler()
@@ -16,7 +17,7 @@ class FCSLogger:
         self.stream_handler.setFormatter(formatter)
         self.logger.addHandler(self.stream_handler)
 
-        self.file_handler = logging.FileHandler(path_to_log_file)
+        self.file_handler = logging.FileHandler(self.path_to_log_file)
         self.file_handler.setLevel(logging.INFO)
         self.file_handler.setFormatter(formatter)
         self.logger.addHandler(self.file_handler)
@@ -32,6 +33,11 @@ class FCSLogger:
         formatter = logging.Formatter(f"{context_name} - %(asctime)s - %(levelname)s - %(message)s")
         self.stream_handler.setFormatter(formatter)
         self.file_handler.setFormatter(formatter)
+
+    def get_log_file_path(self) -> str:
+        """Returns path to log file.
+        """
+        return self.path_to_log_file
 
     def log(self, message):
         self.logger.info(message)
