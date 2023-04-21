c4be7ffce2a8dd1a3fbdf5642c18828ce82cc281
TamasBalogh <tamasbalogh95@gmail.com>
2023-04-21 13:01:29 +0200

    23.4.7.3 - Progress tracker API.

--- FCSViewer.py
+++ FCSViewer.py
@@ -18,7 +18,7 @@ from PyFCS import GEOM_Object
 
 # Versioning check
 from PyFCS import check_api_compatibility, get_backend_api_version
-FCS_PYTHON_API_VERSION = "23.4.7.2"
+FCS_PYTHON_API_VERSION = "23.4.7.3"
 if not check_api_compatibility(FCS_PYTHON_API_VERSION):
     raise Exception(f"Incompatible backend API!\n"
                    f"Please make sure that a major version of {get_backend_api_version()} is used.")
@@ -164,7 +164,7 @@ class FCSViewer(object):
         msg_request = {
             "operation":"finish_progress_tracker",
             "arguments":{
-                "exit_status" : exit_status,
+                "exit_status" : exit_status.value,
                 "exit_message" : exit_message,
                 "immediate_shutdown" : immediate_shutdown
                 }
